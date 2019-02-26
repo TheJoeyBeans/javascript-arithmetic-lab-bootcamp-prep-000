33d3736227310cef33c174e4593d07a0f73e01dd
Joseph  Benbella <benbellajoe@outlook.com>
2019-02-26 01:00:03 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b){
+  return a + b;
+}
+
+add(7,8);
+
+function subtract(a,b){
+  return a - b;
+}
+
+subtract(8,7);
+
+function multiply(a,b){
+  return a * b;
+}
+
+multiply(7,7);
+
+function divide(a,b){
+  return a / b;
+}
+
+divide(840,2);
+
+function inc(n){
+  return n += 1;
+}
+
+inc(5);
+
+function dec(n){
+  return n -= 1;
+}
+
+dec(5);
